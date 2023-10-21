cf0d38bd8019cfd2b938e55a3f48b92c6e9ad8fa
Talaini <talainimohammed@gmail.com>
2023-10-21 13:06:09 +0100

    updates

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Import Module"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    #start function
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run(host='0.0.0.0')
